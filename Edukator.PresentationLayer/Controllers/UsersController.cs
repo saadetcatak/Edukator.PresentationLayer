2abe149aac94da94b22d343de2c72f923559d71c
saadetcatakakgunes1211 <saadetcatak1453@gmail.com>
2023-05-30 22:39:35 +0300

    ders 20

---
+++ Edukator.PresentationLayer/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using Edukator.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Edukator.PresentationLayer.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public UsersController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _userManager.Users.ToList();
+            return View(values);
+        }
+    }
+}
