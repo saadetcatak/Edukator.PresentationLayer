b2249a251a8545504b4f093aec7562402dcd22fc
saadetcatakakgunes1211 <saadetcatak1453@gmail.com>
2023-06-04 21:20:04 +0300

    Final

--- Edukator.PresentationLayer/Controllers/RoleAssignController.cs
+++ Edukator.PresentationLayer/Controllers/RoleAssignController.cs
@@ -1,6 +1,8 @@
 ﻿using Edukator.EntityLayer.Concrete;
+using Edukator.PresentationLayer.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -26,9 +28,41 @@ namespace Edukator.PresentationLayer.Controllers
         public async Task<IActionResult> UserRoleAssign(int id)
         {
             var user = _userManager.Users.FirstOrDefault(x => x.Id == id);
+            TempData["userid"] = user.Id;
             var roles = _roleManager.Roles.ToList();
             var userRoles = await _userManager.GetRolesAsync(user);
-            return View();
+            List<RoleAssignViewModel> roleAssignViewModels= new List<RoleAssignViewModel>();
+            foreach (var item in roles) 
+            { 
+                RoleAssignViewModel model =new RoleAssignViewModel();
+                model.RoleID = item.Id;
+                model.RoleName=item.Name;
+                model.RoleExist = userRoles.Contains(item.Name);
+                roleAssignViewModels.Add(model);
+
+            }
+            return View(roleAssignViewModels);
         }
+        [HttpPost]
+
+         public async Task<IActionResult> UserRoleAssign(List<RoleAssignViewModel>model)
+        {
+            var userid= (int)TempData["userid"];
+            var user = _userManager.Users.FirstOrDefault(x=>x.Id== userid);
+            foreach(var item in model) 
+            { 
+                if (item.RoleExist) 
+                {
+                    await _userManager.AddToRoleAsync(user, item.RoleName);
+                }
+                else
+                {
+                    await _userManager.RemoveFromRoleAsync(user,item.RoleName);
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
---
+++ Edukator.PresentationLayer/Models/UserEditViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Edukator.PresentationLayer.Models
+{
+    public class UserEditViewModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string ImageUrl { get; set; }
+        public IFormFile Image { get; set; }
+
+
+    }
+}
